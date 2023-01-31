024c9a34e51ebf6f0fc545d03ad624e303bbc1a1
MestreLion <linux@rodrigosilva.com>
2023-01-31 09:31:40 -0300

    benchmark: make black and mypy happy

--- benchmark.py
+++ benchmark.py
@@ -9,12 +9,11 @@ import logging
 import os
 import sys
 import time
+import typing as t
 
-# noinspection PyPackageRequirements
-import mss
+import mss  # not listed in requirements, install manually
 import PIL.Image
 import PIL.ImageGrab
-# noinspection PyPackageRequirements
 import pyautogui
 
 
@@ -22,8 +21,9 @@ log = logging.getLogger(os.path.basename(os.path.splitext(__file__)[0]))
 # ------------------------------------------------------------------------------
 
 
-def fps_limit(fps=60):
+def fps_limit(fps: int = 60) -> None:
     from hackmatch.util import FrameRateLimiter
+
     i = 0
     t0 = time.perf_counter()
     timer = FrameRateLimiter(fps)
@@ -38,27 +38,27 @@ def fps_limit(fps=60):
     print(f"{i:6d} frames, {d:.2f}s, {i / d:5.1f} FPS")
 
 
-def getpixel(x=0, y=0):
+def getpixel(x: int = 0, y: int = 0) -> None:
     rgb = PIL.ImageGrab.grab(xdisplay="").getpixel((x, y))
     print(rgb)
 
 
-def bench_ss():
-    def ss_mss():
+def bench_ss() -> None:
+    def ss_mss() -> PIL.Image.Image:
         sct = mss.mss()
         ss = sct.grab(sct.monitors[1])
         return PIL.Image.frombytes("RGB", ss.size, ss.bgra, "raw", "BGRX")
 
-    def ss_pyautogui():
+    def ss_pyautogui() -> PIL.Image.Image:
         return pyautogui.grab()
 
-    def ss_pil_hack():
+    def ss_pil_hack() -> PIL.Image.Image:
         return PIL.ImageGrab.grab(xdisplay="")
 
-    def ss_pil():
+    def ss_pil() -> PIL.Image.Image:
         return PIL.ImageGrab.grab()
 
-    funcs = tuple(v for k, v in locals().items() if callable(v) and k.startswith('ss_'))
+    funcs = tuple(v for k, v in locals().items() if callable(v) and k.startswith("ss_"))
     if not funcs:
         return
 
@@ -69,46 +69,50 @@ def bench_ss():
         benchmark(func)
 
     funcs[0]().show()
+
+
 # ------------------------------------------------------------------------------
-
-
-def benchmark(func, count=100):
-    t0 = time.time()
+def benchmark(func, count: int = 100):  # type: ignore
+    start = time.time()
     for _ in range(count):
         func()
-    t = time.time() - t0
-    fps = count / t
-    avg = 1000 * t / count
-    print(f'{fps:6.2f} FPS, {avg:5.1f}ms avg: {func.__name__}')
+    now = time.time() - start
+    fps = count / now
+    avg = 1000 * now / count
+    print(f"{fps:6.2f} FPS, {avg:5.1f}ms avg: {func.__name__}")
 
 
-def main():
+def main() -> None:
     loglevel = logging.INFO
     # Lame argparse
-    if '-v' in sys.argv[1:]:
+    if "-v" in sys.argv[1:]:
         loglevel = logging.DEBUG
-        sys.argv.remove('-v')
-    logging.basicConfig(level=loglevel, format='%(levelname)-5.5s: %(message)s')
+        sys.argv.remove("-v")
+    logging.basicConfig(level=loglevel, format="%(levelname)-5.5s: %(message)s")
 
-    funcs = tuple(k for k, v in globals().items() if callable(v) and k not in
-                  ('main', 'benchmark'))
+    funcs = tuple(
+        k for k, v in globals().items() if callable(v) and k not in ("main", "benchmark")
+    )
     if len(sys.argv) < 2:
-        print("Usage: {} FUNCTION [ARGS...]\nAvailable functions:\n\t{}".format(
-            __file__, "\n\t".join(funcs)))
+        print(
+            "Usage: {} FUNCTION [ARGS...]\nAvailable functions:\n\t{}".format(
+                __file__, "\n\t".join(funcs)
+            )
+        )
         return
 
     func = sys.argv[1]
-    args = sys.argv[2:]
+    args = t.cast(t.List[object], sys.argv[2:])
     if func not in funcs:
-        log.error("Function %r does not exist! Try one of:\n\t%s",
-                  func, "\n\t".join(funcs))
+        log.error("Function %r does not exist! Try one of:\n\t%s", func, "\n\t".join(funcs))
         return
 
-    def try_int(value):
+    def try_int(value: object) -> object:
         try:
-            return int(value)
+            return int(value)  # type: ignore
         except ValueError:
             return value
+
     args = [try_int(_) for _ in args]
 
     res = globals()[func](*args)
@@ -116,8 +120,8 @@ def main():
         print(repr(res))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        sys.exit(main())
+        main()
     except KeyboardInterrupt:
         pass
